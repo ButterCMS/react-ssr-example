a52e738c7767d044fa023ec8a00ed3fb4d313a4f
fomenyesu <fomenyesu@gmail.com>
2018-01-10 09:47:57 +0800

    json-loader needed
    update webpack config , json-loader needed to start it

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ const config = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel'
+      },{
+        test: /\.json$/,
+        loader: 'json'
       }
     ]
   },
